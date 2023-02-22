166543a714bf3838ad8da4e4c3ddcf7e54e6f783
Rhama Krisner <rhamakrisner@gmail.com>
2023-02-22 16:57:29 -0300

    Modificado o view.fxml

--- src/main/java/com/example/mycep/Application.java
+++ src/main/java/com/example/mycep/Application.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
